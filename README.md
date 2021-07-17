11b37929674b1ad7181e89f491bcb928db8c3ef2
flpcan <felipecandia@live.com>
2021-07-17 18:18:08 -0400

    second commit

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # open_file
 
-Open a simple file in the same folder of the project. 
+Open a simple file in the same folder of the project.
 
 Just for learning
 
+
+We read the file with the file::open and then assign this values
+to the contents empty string. We assign as read_to_string
+Then print the file contents \n for new line
--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,16 @@
-:|D
+use std::fs::File;
+use std::io::prelude::*;
+
+// We read the file with the file::open and then assign this values
+// to the contents empty string. We assign as read_to_string
+// Then print the file contents \n for new line
+fn main() {
+    let mut file = File::open("info.txt").expect("Can't open file!");
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect("Can not read the file...");
+
+    println!("FIle Contents: \n\n {}",contents);
+
+}
